98766e3c340d226c271c98ba19e80b794b1b9725
lesadisa <VbytvFek2003Zhfnfv!>
2021-10-18 22:08:54 +0300

    18012021end

---
+++ app/src/main/java/com/example/feature/Base/Utils.kt
@@ -0,0 +1,8 @@
+package com.example.feature.Base
+
+
+inline fun <reified T> attempt(func: () -> T): Either<Throwable, T> = try {
+    Either.Right(func.invoke())
+} catch (e: Throwable) {
+    Either.Left(e)
+}
--- app/src/main/java/com/example/feature/domain/NewsInteractor.kt
+++ app/src/main/java/com/example/feature/domain/NewsInteractor.kt
@@ -3,7 +3,9 @@ package com.example.feature.domain
 import com.example.feature.data.api.NewsRepo
 import com.example.feature.domain.model.NewsDomainModel
 
-class NewsInteractor(private val newsRepo: NewsRepo) {
+class NewsInteractor(
+    private val newsRepo: NewsRepo
+    ) {
 
     fun getNews(): List<NewsDomainModel> {
         return newsRepo.getNews()
